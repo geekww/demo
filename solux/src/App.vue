1d5aa711aa9cf715a69ee8093f2b93a2429b828d
wangwei <jike_wangwei@163.com>
2019-01-31 14:54:06 +0800

    vue 松霖商城

---
+++ solux/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Cart></Cart>
+    <Footer></Footer>
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import Cart from './components/Cart.vue'
+  import Footer from './components/Footer.vue'
+
+  export default {
+    components:{Header,Cart,Footer},
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
